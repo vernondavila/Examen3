7c788171198ff7fbce51fe163754069fd3eaf6bd
vernondavila <vernon.davila15877@est.uca.edu.ni>
2023-06-05 08:30:25 -0600

    se agrego la descripcion del ejercicio

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,18 @@
+/*Ejercicio: Cálculo de factorial
+ Descripción: Escribe un programa que reciba como entrada un número entero y calcule su
+factorial.
+ Requisitos:
+ - Utilizar una función para calcular el factorial.
+ - Utilizar un ciclo para realizar la multiplicación sucesiva.
+ - Utilizar una bifurcación para manejar el caso de un número negativo.*/
+
 #include <iostream>
 
 using namespace std;
 
 long long calcularFactorial(int numero) {
     if (numero < 0) {
-        cout << "Error: El factorial no está definido para números negativos." << endl;
+        cout << "Error: El factorial no está definido para numeros negativos." << endl;
         return 0;
     }
 
@@ -19,7 +27,7 @@ long long calcularFactorial(int numero) {
 int main() {
     int numero;
 
-    cout << "Ingrese un número entero: ";
+    cout << "Ingrese un numero entero: ";
     cin >> numero;
 
     long long factorial = calcularFactorial(numero);
